923790ebb6e0df01b57d8635327de4c7b0301ab6
johnny-fu <65611863+johnny-fu@users.noreply.github.com>
2024-08-21 13:37:03 +0800

    20240821
    新增精通二技、每日+每周、高山副本、公會城 碎片獎勵計算

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # 新楓之谷 六轉碎片計算器
 由vue3 + vite + bootstrap5 撰寫  
-新增rwd 方便手機點選(應該ㄅ)
+
 計算六轉技能進度與計算累積打多少碎片
+
+rwd 方便手機點選(應該ㄅ)
+每日+每周、高山副本、公會城 碎片獎勵計算
+!!以不缺靈魂方式計算
+周王+活動靈魂愛爾達送蠻多的，應該不會缺
 ## Links
 
 網頁地址 [六轉碎片計算器](https://johnny-fu.github.io/mapleStoryHexa/hexaSkill.html).
@@ -9,5 +14,6 @@
 在 skill.vue 底下  
 const skill_core = ref([0])  
 const proficient_core = ref([0])  
-const strengthen_core = ref([0, 0, 0, 0])  
+const strengthen_core = ref([0, 0, 0, 0])
+const common_core = ref([0])
 於陣列中新增 0 即可加該技能數量
--- src/page/hexaskill/components/skill.vue
+++ src/page/hexaskill/components/skill.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="container">
-        <img class="img-thumbnail mb-3" src="../../../assets/new.png">
+        <img v-if="RogerSaidYouAre2486" src="../../../assets/2486.jpg">
+        <img v-else-if="tureOrNot5555" src="../../../assets/5555.jpg">
+        <img v-else-if="godton7414" src="../../../assets/7414.jpg">
+        <img v-else class="img-thumbnail mb-3" src="../../../assets/newage.jpg">
         <div class="row">
             <div class="col-12 col-md-7 col-lg-7 border-end">
                 <div v-for="(v, i) in skill_core" :key="i" class=" mb-1">
@@ -45,6 +48,21 @@
                         </div>
                     </div>
                 </div>
+                <div v-for="(v, i) in common_core" :key="i" class="mb-1">
+                    <div class="p-2">
+                        <div class="mb-2"> 共用核心{{ i + 1 }}:</div>
+                        <div class="row">
+                            <div class="col-12 col-sm-6 col-md-5 mb-2">
+                                <input type="range" class="form-range p-2" min="0" max="30" step="1" id="common_core"
+                                    v-model="common_core[i]">
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-7">
+                                <input type="number" class="w-25" v-model="common_core[i]" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button type="button" class="btn btn-danger" @click="clear">清空</button>
             </div>
             <div class="col-12 col-md-5 col-lg-5">
                 <div class="p-3">
@@ -60,26 +78,108 @@
                 <div class="p-3">
                     <div class="d-flex text-nowrap">
                         <!-- <div class="col"> -->
-                            背包碎片 : &nbsp;
+                        背包碎片 : &nbsp;
                         <!-- </div> -->
                         <!-- <div class="col"> -->
-                            <input type="number" class="w-25" v-model="bag_fragment" />
+                        <input type="number" class="w-25" v-model="bag_fragment" />
                         <!-- </div> -->
                     </div>
                 </div>
                 <div class="p-3">
-                    <div class="row">
+                    <div class="row mb-2">
                         <div class="col-12 col-lg-6 mb-2 text-nowrap">碎片進度 : {{ roundTo(progess, 2) }}%</div>
-                        <div class="col-12 col-lg-6 text-nowrap">只打每日還有 : {{ Math.ceil(remaining_daily) }}天</div>
+                        <div class="col-12 col-lg-6 text-nowrap">距離出獄還有 : {{
+            remaining_daily != -1 ? Math.ceil(remaining_daily) + "天" : "遙遙無期" }}</div>
+                    </div>
+                    <div class="row">
+                        <div class="form-check col-12 col-md-6 col-lg-4  mb-1">
+                            <input class="form-check-input" type="checkbox" value="daily" id="dailyCheck"
+                                v-model="otherfactor">
+                            <label class="form-check-label" for="dailyCheck">
+                                每日 + 每周
+                            </label>
+                        </div>
+                        <div class="form-check col-12 col-md-6 col-lg-4 mb-1">
+                            <input class="form-check-input" type="checkbox" value="dungeon" id="dungeonCheck"
+                                v-model="otherfactor">
+                            <label class="form-check-label" for="dungeonCheck">
+                                高山副本
+                            </label>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault" value=0
+                                    v-model="dungeon" id="flexRadioDefault1">
+                                <label class="form-check-label" for="flexRadioDefault1">
+                                    免費
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault" value=1
+                                    v-model="dungeon" id="flexRadioDefault2">
+                                <label class="form-check-label" for="flexRadioDefault2">
+                                    200楓點
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault" value=2
+                                    v-model="dungeon" id="flexRadioDefault3">
+                                <label class="form-check-label" for="flexRadioDefault3">
+                                    360楓點
+                                </label>
+                            </div>
+                        </div>
+                        <div class="form-check col-12 col-md-6 col-lg-4 mb-1">
+                            <input class="form-check-input" type="checkbox" value="guild" id="guildCheck"
+                                v-model="otherfactor">
+                            <label class="form-check-label" for="guildCheck">
+                                公會城
+                            </label>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="guildRadio" value=0 v-model="guild"
+                                    id="guildRadio1">
+                                <label class="form-check-label" for="guildRadio1">
+                                    Lv.1
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="guildRadio" value=1 v-model="guild"
+                                    id="guildRadio2">
+                                <label class="form-check-label" for="guildRadio2">
+                                    Lv.2
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="guildRadio" value=2 v-model="guild"
+                                    id="guildRadio3">
+                                <label class="form-check-label" for="guildRadio3">
+                                    Lv.3
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="guildRadio" value=3 v-model="guild"
+                                    id="guildRadio4">
+                                <label class="form-check-label" for="guildRadio4">
+                                    Lv.4
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="guildRadio" value=4 v-model="guild"
+                                    id="guildRadio5">
+                                <label class="form-check-label" for="guildRadio5">
+                                    Lv.5
+                                </label>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                <div>備註:</div>
+                <div class="text-danger">公會城的碎片取最多能獲得量，請相信自己是歐洲人</div>
+                <div class="text-danger">!!出獄進度以不缺靈魂方式計算</div>
+                <div class="text-danger">!!出獄進度以不缺靈魂方式計算</div>
+                <div class="text-danger">!!出獄進度以不缺靈魂方式計算</div>
             </div>
         </div>
-
-
-
-
     </div>
+
 </template>
 
 <script setup>
@@ -90,17 +190,34 @@ const proficient_core_fragments = [50, 15, 18, 20, 23, 25, 28, 30, 33, 100, 40, 
 const proficient_core_soul = [3, 1, 1, 1, 1, 1, 1, 2, 2, 5, 2, 2, 2, 2, 2, 2, 2, 2, 3, 8, 3, 3, 3, 3, 3, 3, 3, 3, 3, 10]
 const strengthen_core_fragments = [75, 23, 27, 30, 34, 38, 42, 45, 49, 150, 60, 68, 75, 83, 90, 98, 105, 113, 120, 263, 128, 135, 143, 150, 158, 165, 173, 180, 188, 375]
 const strengthen_core_soul = [4, 1, 1, 1, 2, 2, 2, 3, 3, 8, 3, 3, 3, 3, 3, 3, 3, 3, 4, 12, 4, 4, 4, 4, 4, 5, 5, 5, 6, 15]
+const common_core_fragments = [125, 38, 44, 50, 57, 63, 69, 75, 82, 300, 110, 124, 138, 152, 165, 179, 193, 207, 220, 525, 234, 248, 262, 275, 289, 303, 317, 330, 344, 750]
+const commo_core_soul = [7, 2, 2, 2, 3, 3, 3, 5, 5, 14, 5, 5, 6, 6, 6, 6, 6, 6, 7, 17, 7, 7, 7, 7, 7, 9, 9, 9, 10, 20]
+const dungeon_fragments = [30, 90, 150]
+const guild_fragments = [23, 26, 29, 32, 35]
 
 const skill_core = ref([0])
-const proficient_core = ref([0])
+const proficient_core = ref([0,0])
 const strengthen_core = ref([0, 0, 0, 0])
+const common_core = ref([0])
 const fragments = ref(0)
 const soul = ref(0)
 const progess = ref(0)
 const bag_fragment = ref(0)
 const remaining_daily = ref(0)
+const otherfactor = ref(["daily"])
+const dungeon = ref(0)
+const guild = ref(0)
+
 var all_fragment = 0
 var all_soul = 0
+
+//不重要
+const RogerSaidYouAre2486 = ref(false)
+const tureOrNot5555 = ref(false)
+const godton7414 = ref(false)
+//
+
+
 
 function count() {
     fragments.value = 0
@@ -129,6 +246,14 @@ function count() {
             countStrengthen(num)
         }
     });
+    common_core.value.forEach(num => {
+        if (num < 0) {
+        } else if (num > 30) {
+            countCommon(30)
+        } else {
+            countCommon(num)
+        }
+    });
 }
 function countSkill(x) {
     for (var i = 0; i < x; i++) {
@@ -148,17 +273,81 @@ function countStrengthen(x) {
         soul.value = soul.value + strengthen_core_soul[i];
     }
 }
+function countCommon(x) {
+    for (var i = 0; i < x; i++) {
+        fragments.value = fragments.value + common_core_fragments[i];
+        soul.value = soul.value + commo_core_soul[i];
+    }
+}
 
 function countDailyAndProgess() {
-    var temp = 0;
+    var tempBagFragment = 0;
+    var remainingDay = 0;
+    var remaining = 0;
+
     if (bag_fragment.value > 0) {
-        temp = bag_fragment.value
+        tempBagFragment = bag_fragment.value
     }
-    remaining_daily.value = (all_fragment - (fragments.value + temp)) / 12
-    progess.value = (fragments.value + temp) / all_fragment * 100
+    remaining = all_fragment - (fragments.value + tempBagFragment)
+
+    if (otherfactor.value.length == 0) {
+        remainingDay = -1
+    } else {
+        remainingDay = calremainingDay(remaining)
+    }
+
+    //返回剩餘天數 與 進度
+    remaining_daily.value = remainingDay
+
+    progess.value = (fragments.value + tempBagFragment) / all_fragment * 100
     if (progess.value > 100) {
         progess.value = 100
     }
+}
+
+function calremainingDay(remaining) {
+    var remainingDay = 0;
+    var date = new Date().getDay();
+    var weekcomplete = false
+    var dungeoncomplete = false
+    var guildcomplete = false
+    //計算剩餘天數
+    //以 剩餘需要碎片 - 每日 
+    //剩餘需要碎片 - 每周 - 高山 
+    while (remaining > 0) {
+        remainingDay++
+
+        if (otherfactor.value.includes('daily')) {
+            remaining = remaining - 12
+            if (date >= 3 && !weekcomplete) {
+                remaining = remaining - 50
+                weekcomplete = true
+            }
+        }
+        if (otherfactor.value.includes('dungeon')) {
+
+            if (!dungeoncomplete) {
+                remaining = remaining - dungeon_fragments[dungeon.value]
+                dungeoncomplete = true
+            }
+        }
+        if (otherfactor.value.includes('guild')) {
+            if (!guildcomplete) {
+                remaining = remaining - guild_fragments[guild.value]
+                guildcomplete = true
+            }
+        }
+        date++
+        if (date == 7) {
+            date = 0
+            weekcomplete = false
+            guildcomplete = false
+        }
+        if (date == 3) {
+            dungeoncomplete = false
+        }
+    }
+    return remainingDay
 }
 
 function checkAll() {
@@ -173,18 +362,67 @@ function checkAll() {
     strengthen_core.value.forEach(num => {
         countStrengthen(30)
     });
+    common_core.value.forEach(num => {
+        countCommon(30)
+    });
     all_fragment = fragments.value
     all_soul = soul.value
     fragments.value = 0
     soul.value = 0
 }
 
+function clear() {
+    fragments.value = 0
+    soul.value = 0
+    for (var i = 0; i < skill_core.value.length; i++) {
+        skill_core.value[i] = 0
+    }
+    for (var i = 0; i < proficient_core.value.length; i++) {
+        proficient_core.value[i] = 0
+    }
+    for (var i = 0; i < strengthen_core.value.length; i++) {
+        strengthen_core.value[i] = 0
+    }
+    for (var i = 0; i < common_core.value.length; i++) {
+        common_core.value[i] = 0
+    }
+    otherfactor.value = []
+    dungeon.value = 0
+    guild.value = 0
+}
+
 const roundTo = function (num, decimal) { return Math.round((num + Number.EPSILON) * Math.pow(10, decimal)) / Math.pow(10, decimal); }
-watch([skill_core.value, proficient_core.value, strengthen_core.value], async (newVal, oldVal) => {
+watch([skill_core.value, proficient_core.value, strengthen_core.value, common_core.value], async (newVal, oldVal) => {
     count()
     countDailyAndProgess()
 })
 watch(bag_fragment, () => {
+    countDailyAndProgess()
+    switch (bag_fragment.value) {
+        case 2486:
+            RogerSaidYouAre2486.value = true
+            break
+        case 555:
+            tureOrNot5555.value = true
+            break
+        case 7414:
+            godton7414.value = true
+            break
+        default:
+            RogerSaidYouAre2486.value = false
+            tureOrNot5555.value = false
+            godton7414.value = false
+            break
+    }
+    
+})
+watch(otherfactor, () => {
+    countDailyAndProgess()
+})
+watch(dungeon, () => {
+    countDailyAndProgess()
+})
+watch(guild, () => {
     countDailyAndProgess()
 })
 onMounted(() => {
